af00f64d042c9e5ca28969eed73ef12df7a6647e
JiHyeon Noh <ggpji59105@naver.com>
2023-02-04 19:37:51 +0900

    2023/02/04

--- Step by step/Stage 11 재귀/25501_ 재귀의 귀재.py
+++ Step by step/Stage 11 재귀/25501_ 재귀의 귀재.py
@@ -1,2 +1,17 @@
 # 재귀의 동작을 파악하는 문제
 # -----------------------------------------------------------------------------------------
+def recursion(s, l, r):
+    global n
+    n += 1
+    if l >= r: return 1
+    elif s[l] != s[r]: return 0
+    else: return recursion(s, l+1, r-1)
+
+def isPalindrome(s):
+    return recursion(s, 0, len(s)-1)
+
+N = int(input())
+for i in range(N) :
+    s = input()
+    n = 0
+    print(isPalindrome(s), n, end=' ')
