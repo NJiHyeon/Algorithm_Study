573d1717a94469217842872dff23c9087fa49835
JiHyeon Noh <ggpji59105@naver.com>
2023-02-07 20:39:46 +0900

    2023/02/07

--- Step by step/Stage 11 재귀/2447_ 별 찍기 - 10.py
+++ Step by step/Stage 11 재귀/2447_ 별 찍기 - 10.py
@@ -39,23 +39,21 @@ for i in range(n) :
 import sys
 sys.setrecursionlimit(10**6)
 
-def append_star(LEN) :
-    if LEN == 1 :
+def cycle_star(s) :
+    if s == 1 :
         return ['*']
     
-    Stars = append_star(LEN//3)
+    star = cycle_star(s//3)
     L = []
     
-    for S in Stars :
-        L.append(S*3)
-        
-    for S in Stars :
-        L.append(S + ' ' * (LEN//3) + S)
-    
-    for S in Stars :
-        L.append(S*3)
+    for i in star :
+        L.append(i*3)
+    for i in star :
+        L.append(i + ' '*s//3 + i)
+    for i in star :
+        L.append(i*3)
     
     return L
 
-n = int(sys.stdin.readline().strip())
-print('\n'.join(append_star(n)))
+s = int(sys.stdin.readline().strip())
+print('\n', cycle_star(s))
