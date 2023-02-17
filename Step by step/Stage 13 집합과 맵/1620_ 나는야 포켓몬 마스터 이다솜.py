b7d57d62cca55ed3f8b550ed021edacff68a0bf1
JiHyeon Noh <ggpji59105@naver.com>
2023-02-17 21:58:04 +0900

    2023/02/17

--- Step by step/Stage 13 집합과 맵/1620_ 나는야 포켓몬 마스터 이다솜.py
+++ Step by step/Stage 13 집합과 맵/1620_ 나는야 포켓몬 마스터 이다솜.py
@@ -1,1 +1,33 @@
 # 맵을 사용하여 이름과 수를 연결짓는 문제
+#------------------------------
+# 처음 시도한 코드
+import sys
+N, M = map(int, sys.stdin.readline().split())
+p_list = []  
+for i in range(N+M) :
+    if i < N : 
+        name = input()
+        p_list.append(name)
+    else : 
+        q = input()
+        if q in p_list : 
+            print(p_list.index(q) + 1)
+        else : 
+            print(p_list[int(q)-1])
+        
+#------------------------------
+# 딕셔너리로 풀기
+import sys
+N, M = map(int, sys.stdin.readline().split())
+p_dict = {}
+for i in range(N) :
+    name = input().rstrip()
+    p_dict[i] = name
+    p_dict[name] = i
+    
+for i in range(M) :
+    q = input().rstrip()
+    if q.isdigit() :
+        print(p_dict[int(q)-1])
+    else : 
+        print(p_dict[q]+1)
