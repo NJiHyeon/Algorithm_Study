2b7c79c88a140a0659855a0f3eed46eed4836762
NJiHyeon <ggpji59105@naver.com>
2023-02-21 14:52:58 +0900

    2023/02/21

--- Step by step/Stage 13 집합과 맵/11478_ 서로 다른 부분 문자열의 개수.py
+++ Step by step/Stage 13 집합과 맵/11478_ 서로 다른 부분 문자열의 개수.py
@@ -1,1 +1,13 @@
-DD
+# 집합을 활용하여 중복을 제거하는 문제
+# ------------------------------------------------------
+import sys
+
+S = sys.stdin.readline().strip()
+cha = []
+for i in range(1, len(S)+1) :
+    for j in range(len(S)) :
+        s = S[j:j+i]
+        cha.append(s)
+
+
+print(len(set(cha)))
