b5705c90c601a672a9251d0db2c9966bc82f08aa
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2023-10-07 12:57:20 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -20,3 +20,15 @@ def solution(s):
     if "nine" in s :
         s = s.replace("nine", "9")
     return int(s)
+
+
+# 좋은 풀이법
+'''
+numbers = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+
+def solution(s) :
+    answer = s
+    for k, v in number.items() :
+        answer = answer.replace(k, v)
+    return int(answer)
+'''
