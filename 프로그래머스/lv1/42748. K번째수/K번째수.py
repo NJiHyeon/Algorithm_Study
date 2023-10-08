0f9d44e0a0bde67795f4bb399e7526ee438f729c
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2023-10-08 21:22:02 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,11 +1,7 @@
 def solution(array, commands):
     answer = []
     for c in commands :
-        # i = 2, j = 5, k = 3
-        i = c[0] - 1
-        j = c[1]
-        k = c[2] - 1
-        arr = array[i : j]
+        arr = array[c[0]-1 : c[1]]
         arr.sort()
-        answer.append(arr[k])
+        answer.append(arr[c[2]-1])
     return answer
