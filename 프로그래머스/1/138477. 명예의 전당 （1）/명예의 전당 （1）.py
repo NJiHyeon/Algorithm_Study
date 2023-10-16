f02065a4794e4e63bdb07af041c330e4ea7588a8
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2023-10-16 20:18:56 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -2,10 +2,12 @@ def solution(k, score):
     top = []
     ann = []
     for i in range(len(score)) :
+        # i가 k보다 작으면 top에서 제거없이 계속 추가
         if i < k :
             top.append(score[i])
             top.sort(reverse=True)
             ann.append(top[-1])
+        # i가 k보다 크거나 같아지면 제일 점수가 작은 것 제거 후 추가해서 sort
         else :
             if score[i] > top[-1] : 
                 top.pop(-1)
@@ -15,21 +17,5 @@ def solution(k, score):
                 
             else :
                 ann.append(top[-1])
-    
 
-    '''
-    for i in range(k) :
-        top.append(score[i])
-        top.sort(reverse=True)
-        ann.append(top[-1])
-    
-    for i in range(k, len(score)) :
-        if score[i] > top[-1] : 
-            top.pop(-1)
-            top.append(score[i])
-            top.sort(reverse=True)
-            ann.append(top[-1])
-        else :
-            ann.append(top[-1])
-    '''
     return ann
