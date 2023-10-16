b814b5ee8bf459cd2beeebb29bdb487887059fb3
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2023-10-16 19:52:23 +0900

    [level 1] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,15 +1,12 @@
 def solution(a, b, n):
-    answer = 0
+    answer = 0 # 총 콜라병을 받는 개수
+    # 남은 콜라병이 마트에 주어야할 개수 (a)보다 작으면 끝
     while True :
-        take = n // a * b # 6
-        answer += take   # 6
-        
-        remain = n % a # 2
-        
-        n = take + remain
-        
+        take = n // a * b # 마트에서 콜라병을 받는 개수
+        answer += take   # 최종 개수에 더하기
+        remain = n % a # 나누어 떨어지지 않고 그대로 집에 가져오는 콜라병의 개수
+        n = take + remain 
         if n < a : 
             break
-        
         
     return answer
