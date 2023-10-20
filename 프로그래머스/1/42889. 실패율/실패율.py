d61366f220858b637bd33946828524b4b4f0ba6d
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2023-10-20 22:38:16 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -2,8 +2,10 @@ def solution(N, stages):
     fail = []
     l = len(stages)
     for i in range(1, N+1) : # 1단계부터 N단계까지 실패율
-        if l == 0 :
+        # 스테이지에 도달한 유저가 없는 경우
+        if l == 0 : 
             fail.append(0)
+        # 스테이지에 도달한 유저가 있는 경우
         else :
             a = stages.count(i)
             fail.append(a/l)
