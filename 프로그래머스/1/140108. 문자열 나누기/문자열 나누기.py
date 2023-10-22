abb90f3947b884619d1be50ae3571e74e883aed4
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2023-10-23 06:48:00 +0900

    [level 1] Title: 문자열 나누기, Time: 3.84 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,18 +1,18 @@
 def solution(s):
     i = na = nb = 0
-    answer = 0
+    answer = 0 # 출력값
     for j in range(len(s)) :
-        if len(s[j:]) == 1 :
+        if len(s[j:]) == 1 : # 마지막번째 문자면 answer에 1 더하고 끝.
             answer += 1
             break
         
-        l = s[i] #기준 : b
-        if s[j] == l : #b==b
+        l = s[i] #기준정하기
+        
+        if s[j] == l : 
             na += 1
             if na == nb :
                 i = j+1
                 answer += 1
-            
         else : 
             nb += 1
             if na == nb :
