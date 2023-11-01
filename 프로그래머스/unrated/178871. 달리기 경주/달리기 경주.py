b1c7bda9351e633aa57c830b86c7cd824890ae2a
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2023-11-01 16:00:58 +0900

    Update 달리기 경주.py

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -1,34 +1,12 @@
-# 해결방법
-'''
-1. calling에서 순서대로 이름들을 호출하고,
-이름이 호출되면 그 이름의 인덱스를 찾는다. 
-2. 해당 인덱스랑 현재-1번째 인덱스랑 위치 바꾸기 (players)
-(시간초과 해결) 리스트 -> 딕셔너리
-'''
-#players = ["mumu", "soe", "poe", "kai", "mine"]
-#callings = ["kai", "kai", "mine", "mine"]
-#result = ["mumu", "kai", "mine", "soe", "poe"] # 출력돼야함
 def solution(players, callings) :
-    players_rank = [i for i in range(1, len(players)+1)]
-    dic = dict(zip(players, players_rank))
-    dic_rev = dict(map(reversed, dic.items()))
-    for i in range(0, len(callings)) :
-        win_name = callings[i] # kai
-        win_rank = dic[win_name] # 4
-        lose_rank = win_rank - 1  # 3
-        lose_name = dic_rev[lose_rank] # poe
+    play = {name:index for index, name in enumerate(players)}
+    for cal in callings :
+        i = play[cal] #현재등수
+        players[i-1], players[i] = players[i], players[i-1]#등수에 따라 사람 바꾸기
+        #현재등수 바꾸기
+        play[players[i-1]] = i-1
+        play[players[i]] = i
         
-        dic[win_name] = lose_rank
-        dic[lose_name] = win_rank
-        dic_rev[lose_rank] = win_name
-        dic_rev[win_rank] = lose_name
-        
-    d = sorted(dic_rev.items())
-    dd = []
-    for i in range(len(d)) :
-        dd.append(d[i][1])
-    
-    return dd
-
+    return players
 
     
