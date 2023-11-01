dcb6dea1ca3b6a6d9d3964691a8acf1ecf1dee57
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2023-11-01 14:50:13 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.33 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -4,14 +4,15 @@ def solution(today, terms, privacies):
     
     for i in range(len(privacies)) :
         for t in terms :
-            if t[0] in privacies[i] : #"Z" in "2019.01.01 D"
-                trash_month = int(t[2:]) + int(privacies[i][5:7]) #'6'+'05' -> 6+5=11
+            if t[0] in privacies[i] : 
+                trash_month = int(t[2:]) + int(privacies[i][5:7]) 
+                # 더했을때 12월의 배수이면 조심해야함 !
                 if trash_month%12==0 :
                     new_month = 12
                     new_year = int(privacies[i][:4]) + (trash_month // 12) - 1
                 else :
-                    new_month = trash_month % 12 #11
-                    new_year = int(privacies[i][:4]) + (trash_month // 12) #2021+0=2021
+                    new_month = trash_month % 12 
+                    new_year = int(privacies[i][:4]) + (trash_month // 12) 
                 trash_day = ''
                 if len(str(new_month))==1 :
                     trash_day = str(new_year)+'.0'+str(new_month)+'.'+privacies[i][8:10]
@@ -20,9 +21,9 @@ def solution(today, terms, privacies):
 
                 trash_list.append(trash_day)
 
-    # today : "2022.05.19"
+    
     today = int(today.replace('.', ''))
-    for j in range(len(trash_list)) : # ['2021.11.02', '2022.07.01', '2022.05.19', '2022.05.20']
+    for j in range(len(trash_list)) : 
         k = int(trash_list[j].replace('.', ''))
         if today >= k :
             answer.append(j+1)
