e424377329776ac8e66a458b2b65f7826a4c371c
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2023-11-05 19:12:09 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.25 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -5,3 +5,9 @@ def solution(A,B):
     for i in range(len(A)) :
         answer += (A[i]*B[i])
     return answer
+
+# 한줄코딩
+'''
+def solution(A, B) :
+    return sum([a*b for a, b in zip(sorted(A), sorted(B, reverse=True))])
+'''
