10a92a840dbef227f77de30b4cca515b9939c6e9
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2023-11-13 21:49:21 +0900

    [level 2] Title: 귤 고르기, Time: 20.69 ms, Memory: 18 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -3,13 +3,13 @@ def solution(k, tangerine):
     answer = 0 #서로 다른 종류의 수
     n = 0 #k와 비교할 수
     dict = collections.Counter(tangerine)
-    dict = sorted(dict.items(), key=lambda x: x[1], reverse=True)
-    if dict[0][1] > k :
+    dict = sorted(dict.values(), reverse=True)
+    if dict[0] > k :
         return 1
     else :
         for i in range(len(dict)) :
-            if n+dict[i][1] < k :
-                n+=dict[i][1]
+            if n+dict[i] < k :
+                n+=dict[i]
                 answer+=1
             else :
                 return answer+1
