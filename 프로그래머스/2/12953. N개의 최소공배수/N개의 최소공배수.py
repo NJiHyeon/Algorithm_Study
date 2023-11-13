f78073630e63241bfbe80eae9e40cf37ed79b4cc
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2023-11-13 19:56:12 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,8 +1,7 @@
 def solution(arr):
     from math import gcd
-    arr.sort()
-    start = arr[0] #12
+    start = arr[0] 
     for i in arr[1:] :
-        g = gcd(start, i) #4
-        start = g*(start//g)*(i//g)
+        g = gcd(start, i) 
+        start = g*(start//g)*(i//g) #start*i//g
     return start
