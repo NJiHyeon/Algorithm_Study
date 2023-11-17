2abc402cb8b7f529d8f0f68ac71912e5a351a247
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2023-11-17 16:37:00 +0900

    Update 기능개발.py

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -10,24 +10,8 @@ def solution(progresses, speeds):
         if len(stack) == 0 :
             stack.append(d)
         else :
-            '''
-            # 3이 있을 때 3이 들어올 때
-            if stack[0] == stack[-1] == d :
-                answer.append(1)
-                stack.pop()
-                stack.append(d)
-            # 10, 3이 있을 때 3이 들어올 때
-            elif (stack[0]!=stack[-1]) and (stack[-1]==d) :
-                stack.append(d)
-            if stack[-1] >= d :
-                stack.append(d)
-            # 10이 있을 때 3이 들어올 때
-            elif stack[-1] > d :
-                stack.append(d)
-            '''
             if stack[0] >= d :
                 stack.append(d)
-            # 10이 있을 때 12가 들어올 때
             elif stack[0] < d :
                 answer.append(len(stack))
                 stack.clear()
