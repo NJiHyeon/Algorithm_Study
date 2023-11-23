1dd69f46980543672647dbab8e853afab09fa438
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2023-11-24 01:30:11 +0900

    [level 2] Title: 전화번호 목록, Time: 84.05 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,12 +1,7 @@
 def solution(phone_book):
-    hash_map = {}
-    for phone_number in phone_book:
-        hash_map[phone_number] = 1
+    phone_book.sort()
     
-    for phone_number in phone_book:
-        jubdoo = ""
-        for number in phone_number:
-            jubdoo += number
-            if jubdoo in hash_map and jubdoo != phone_number:
-                return False
+    for i in range(len(phone_book)-1) :
+        if phone_book[i+1].startswith(phone_book[i]) :
+            return False
     return True
