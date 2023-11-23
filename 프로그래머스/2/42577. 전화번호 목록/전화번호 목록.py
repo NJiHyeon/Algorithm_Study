a8d8fced3226b2d030de99e2bd440f302acf6c72
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2023-11-24 01:33:54 +0900

    [level 2] Title: 전화번호 목록, Time: 79.92 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,10 +1,23 @@
 def solution(phone_book):
+    phone_book.sort()
+    for i in range(len(phone_book)-1) :
+        if phone_book[i+1].startswith(phone_book[i]) :
+            return False
+    return True
+
+
+# 해시 개념을 이용해 풀이
+'''
+def solution(phone_book) :
     answer = True
-    for i in range(len(phone_book)) :
-        s = phone_book[i]
-        l = len(s)
-        for j in range(len(phone_book)) :
-            if s == phone_book[j][:l] and i!=j :
-                return False
-        
+    hash_map = {}
+    for phone_number in phone_book :
+        hash_map[phone_number] = 1
+    for phone_number in phone_book :
+        temp = ""
+        for number in phone_number :
+            temp += number
+            if temp in hash_map and temp!= phone_number :
+                answer = False
     return answer
+'''
