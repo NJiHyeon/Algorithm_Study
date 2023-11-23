d679b1402d5582f47c147cd88c13f01f8785418e
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2023-11-24 03:18:01 +0900

    [level 2] Title: 타겟 넘버, Time: 3.57 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,17 +1,3 @@
 def solution(numbers, target):
     answer = 0
-    # 초기값 설정
-    queue = [[numbers[0], 0], [-1*numbers[0], 0]]
-    n = len(numbers) 
-    
-    while queue :
-        temp, idx = queue.pop()
-        idx += 1
-        if idx < n :
-            queue.append([temp+numbers[idx], idx])
-            queue.append([temp-numbers[idx], idx])
-        else :
-            if temp==target :
-                answer += 1
-                
     return answer
