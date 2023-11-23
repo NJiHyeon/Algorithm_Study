e8c2f1cde6444981bbf7df1824c6e60d6aa7df4c
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2023-11-24 03:37:15 +0900

    [level 2] Title: 모음 사전, Time: 1.33 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -0,0 +1,9 @@
+def solution(word):
+    answer = 0
+    from itertools import product
+    words = []
+    for i in range(1, 6) :
+        for c in product(['A', 'E', 'I', 'O', 'U'], repeat = i) :
+            words.append(''.join(list(c)))
+    words.sort()        
+    return words.index(word)+1
