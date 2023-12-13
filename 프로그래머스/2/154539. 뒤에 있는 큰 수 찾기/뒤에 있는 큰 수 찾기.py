879e07af895a936f8c1a61150876a4bb323547f4
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2023-12-13 22:11:13 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 169.02 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
@@ -1,8 +1,12 @@
 def solution(numbers):
-    result = [-1] * len(numbers)
-    stack = [] #아직까지 큰 수를 찾지 못한 인덱스 저장소
+    # 초기 리스트 설정
+    result = [-1]*len(numbers)
+    q = []
     for i in range(len(numbers)) :
-        while stack and numbers[i] > numbers[stack[-1]]: #stack이 비어 있지 않고 큰 수가 나타나면
-            result[stack.pop()] = numbers[i]
-        stack.append(i)
+        while q and numbers[q[-1]] < numbers[i] :
+            result[q[-1]] = numbers[i]
+            q.pop()
+        q.append(i) #숫자를 넣으면 겹치므로 인덱스로 구분
+    
+    
     return result
