e15992d3b3a299fcd95d89a887f8cbf83b93b5cb
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2023-12-22 14:32:25 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -0,0 +1,15 @@
+def solution(skill, skill_trees):
+    answer = 0
+    skilli = []
+    for s in skill_trees :
+        skill_index = []
+        for k in s :
+            if k in skill :
+                skill_index.append(skill.index(k))
+        if len(skill_index) != 0 :
+            big = max(skill_index)
+            if skill_index == [i for i in range(big+1)] :
+                answer += 1
+        else :
+            answer += 1
+    return answer
