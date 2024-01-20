685b1604ba1c912e6c0ab8bdc969419054cfd040
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-01-20 14:00:16 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -3,7 +3,7 @@ def solution(number, k):
     l = len(number)
     for i in range(len(number)) :
         while k>0 and stack and stack[-1]<number[i] :
-            k -= 1
             stack.pop()
+            k -= 1
         stack.append(number[i])
     return ''.join(stack[:l-k])
