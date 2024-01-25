a4742a0a24cec9b6a86a7c0b55bf655873a0b06c
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-01-25 16:56:25 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.26 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,11 +1,12 @@
 def solution(orders, course):
     answer = []
+    # 조건 : 배열의 각 원소에 저장된 문자열 또한 알파벳 오름차순으로 정렬
     for i in range(len(orders)) :
         dd = sorted(orders[i])
         ddd = ''.join(sorted(dd))
         orders[i]=ddd
 
-    from itertools import combinations, permutations
+    from itertools import combinations
     for c in course :
         c_dict = dict()
         for i in range(len(orders)) :
@@ -18,16 +19,19 @@ def solution(orders, course):
                     else :
                         c_dict[name] = 1
         
+        # 제일 많은 순으로 
         c_list = sorted(c_dict.items(), key=lambda x: x[1], reverse=True)
         
+        # 조건 : 최소 2명 이상의 손님으로부터 주문된 단품메뉴 조합 (앞에서 정렬했으므로 첫번째 인덱스 확인)
         if c_list and c_list[0][1] >= 2 :
             answer.append(c_list[0][0])
         else :
             continue
+        # 조건 : 만약 가장 많이 함께 주문된 메뉴 구성이 여러 개라면, 모두 배열에 담아 return 하면 됩니다.
         k = 1
         while len(c_list)>=k+1 and c_list[k][1] == c_list[0][1] :
             answer.append(c_list[k][0])
             k += 1
-            
+    # 조건 : 정답은 각 코스요리 메뉴의 구성을 문자열 형식으로 배열에 담아 사전 순      
     answer.sort()      
     return answer
