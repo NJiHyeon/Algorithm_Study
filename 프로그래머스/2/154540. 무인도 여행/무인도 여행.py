be067d6be9822ac6928310d59154c4b3fa0ae1f0
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-01-30 08:50:46 +0900

    [level 2] Title: 무인도 여행, Time: 0.17 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -1,32 +1,30 @@
-from collections import deque
-dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]
+
+import sys
+sys.setrecursionlimit(int(1e7))
+
+def dfs(maps, i, j, n, m, visit):
+    visit[i][j] = True
+    area = int(maps[i][j])
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    for dx, dy in directions:
+        x, y = i + dx, j + dy
+        if 0 <= x < n and 0 <= y < m and not visit[x][y] and maps[x][y] != 'X':
+            area += dfs(maps, x, y, n, m, visit)
+
+    return area
 
 def solution(maps):
     answer = []
     n, m = len(maps), len(maps[0])
-    visit = [[0] * m for _ in range(n)]
-    
-    for i in range(n) :
-        for j in range(m) :
-            if maps[i][j] != 'X' and visit[i][j] == 0 :
-                answer.append(bfs(maps, i, j, n, m, visit))
-    if answer :
+    visit = [[False] * m for _ in range(n)]
+
+    for i in range(n):
+        for j in range(m):
+            if maps[i][j] != 'X' and not visit[i][j]:
+                answer.append(dfs(maps, i, j, n, m, visit))
+
+    if answer:
         return sorted(answer)
-    else :
+    else:
         return [-1]
-    
-def bfs(maps, i, j, n, m, visit) :
-    queue = deque()
-    queue.append((i,j))
-    visit[i][j] = 1
-    area = int(maps[i][j])
-    while queue :
-        x, y = queue.popleft()
-        for d in range(4) :
-            nx, ny = x+dx[d], y+dy[d]
-            if 0 <= nx < n and 0 <= ny < m and visit[nx][ny] == 0 and maps[nx][ny] != 'X' :
-                visit[nx][ny] = 1
-                area += int(maps[nx][ny])
-                queue.append((nx,ny))
-    return area
-    
