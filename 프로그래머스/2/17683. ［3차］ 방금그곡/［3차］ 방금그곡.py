abe435edf0c357714900bb9a21fe7527ec75c741
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-02-02 17:29:50 +0900

    [level 2] Title: [3차] 방금그곡, Time: 1.51 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
@@ -0,0 +1,51 @@
+def solution(m, musicinfos):
+    answer = '(None)'
+    music = []
+    m = m.replace('C#', 'c')
+    m = m.replace('D#', 'd')
+    m = m.replace('F#', 'f')
+    m = m.replace('G#', 'g')
+    m = m.replace('A#', 'a')
+
+    for mm in musicinfos :
+        mu = mm.split(',')
+        mu[3] = mu[3].replace('C#', 'c')
+        mu[3] = mu[3].replace('D#', 'd')
+        mu[3] = mu[3].replace('F#', 'f')
+        mu[3] = mu[3].replace('G#', 'g')
+        mu[3] = mu[3].replace('A#', 'a')
+        l = t(mu[0],mu[1])
+        
+        if mel(m, mu[3], l) == True :
+            new = []
+            new.append(l)
+            new.append(mu[0])
+            new.append(mu[2])
+            music.append(new)
+            
+    if len(music) == 0 :
+        return "(None)"
+    else :
+        music.sort(key = lambda x:x[1])
+        music.sort(key = lambda x:x[0], reverse=True)
+        return music[0][2]
+
+def t(m0, m1) :
+    if int(m1[3:]) - int(m0[3:]) >= 0 :
+        h = int(m1[0:2]) - int(m0[0:2])
+        m = int(m1[3:]) - int(m0[3:])
+    else :
+        h = int(m1[0:2]) - int(m0[0:2]) - 1
+        m = 60 + int(m1[3:]) - int(m0[3:])
+    mm = m + (h*60)
+    return mm
+    
+def mel(m, mu, l) :
+    a = l//len(mu)
+    b = l%len(mu)
+    c = mu*a + mu[:b]
+    if m in c :
+        return True
+    else :
+        return False
+    
