38cdecd78197208db581a073e7f33710f4b13039
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-02-15 17:50:34 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 233.37 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -1,9 +1,8 @@
 def solution(rows, columns, queries):
-    import copy
     answer = []
     # 1. init metrics 
-    metric = [[((i-1)*columns + j) for j in range(1, columns+1)] for i in range(1, rows+1)]
-    rotate_metric = [[((i-1)*columns + j) for j in range(1, columns+1)] for i in range(1, rows+1)]
+    metric = [[((i-1)*columns + j) for j in range(1, columns+1)] for i in range(1, rows+1)] #query 한번 돌때마다 갱신
+    rotate_metric = [[((i-1)*columns + j) for j in range(1, columns+1)] for i in range(1, rows+1)] #각 행을 돌때마다 갱신
 
     # 2. for문 돌면서 query 적용
     for q in queries: #q = [2,2,5,4]
@@ -25,8 +24,7 @@ def solution(rows, columns, queries):
                 rotate_metric[i][q[3]-1] = metric[i-1][q[3]-1]
                 if m > rotate_metric[i][q[1]-1] or m > rotate_metric[i][q[3]-1] :
                     m = min(rotate_metric[i][q[1]-1], rotate_metric[i][q[3]-1])
-                
-        #metric = copy.deepcopy(rotate_metric)
-        metric = [item[:] for item in rotate_metric]
+
+        metric = [item[:] for item in rotate_metric] #metric은 하나의 query 내에서는 고정되어야 하고, 하나의 query가 끝나면(즉, 행을 다 돌면) 다음 쿼리로 넘겨주어야 하기 때문에 최종 rotate_metric으로 교체
         answer.append(m)
     return answer
