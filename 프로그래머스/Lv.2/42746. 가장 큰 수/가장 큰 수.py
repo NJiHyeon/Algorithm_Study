ac76ef86058823b1c0563475554e18b500ad0807
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-02-16 20:15:57 +0900

    Delete 프로그래머스/Lv.2 directory

--- 프로그래머스/Lv.2/42746. 가장 큰 수/가장 큰 수.py
+++
@@ -1,5 +0,0 @@
-def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(key = lambda x : x*3, reverse=True) #1000이하의 수이므로, 최소1/최대3의 자리수니까
-    answer = ''.join(numbers)
-    return str(int(answer))
