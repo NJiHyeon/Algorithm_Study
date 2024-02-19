1e2793b858e7d0a485b7122878b4c8e80eed8de9
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-02-19 16:26:12 +0900

    Update 줄 서는 방법.py

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -19,3 +19,27 @@ def solution(n, k):
             break
     answer += origin[::-1]
     return answer
+
+
+# 시간초과
+    from itertools import permutations
+    jul = []
+    j = 0
+    for i in permutations([i for i in range(1, n+1)], n):
+        j += 1
+        if j == k :
+            return i
+
+# 다른 풀이
+from math import factorial
+    answer = []
+    order = list(range(1,n+1))
+    m = []
+    while n!=0 :
+        fact = factorial(n-1)
+        #answer.append(order.pop(k//fact-1 if k%fact ==0 else k//fact))
+        m.append([k-1, fact, (k-1)//fact])
+        answer.append(order.pop((k-1)//fact))
+        n,k = n-1, k%fact
+    return m
+    return answer
