ab96b6cb970a920026cd074753d9f1e5c8459c60
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-06-19 13:44:43 +0900

    Update 1472. Design Browser.py

--- LeetCode/1472. Design Browser.py
+++ LeetCode/1472. Design Browser.py
@@ -8,18 +8,15 @@ class BrowserHistory:
     def __init__(self, homepage: str):
         self.head = Node(val=homepage)
         self.current = Node(val=homepage)
-
     def visit(self, url: str) -> None:
         new_node = Node(val=url, prev=self.current)
         self.current.next = new_node
         self.current = self.current.next
-
     def back(self, steps: int) -> str:
         while steps > 0 and self.current.prev != None :
             steps -= 1
             self.current = self.current.prev
         return self.current.val
-
     def forward(self, steps: int) -> str:
         while steps > 0 and self.current.next != None :
             steps -= 1
