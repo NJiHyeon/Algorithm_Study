7694bad86b35acbac7da6008dc976d90578a2ee3
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-07-25 03:11:08 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.16 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,42 +1,49 @@
 def solution(places):
     from collections import deque
-    def inRange(r, c):
+    def in_Range(r, c):
         return 0 <= r < 5 and 0 <= c < 5
     
-    def bfs(i, j, place):
-        dr = [0, -1, 0, 1]
+    # bfs code
+    def bfs(r, c, place):
+        dr = [0, 1, 0, -1]
         dc = [1, 0, -1, 0]
-        queue = deque()
-        queue.append((i, j, 0))
+        q = deque()
+        q.append((r, c, 0))
         visited = [[False]*5 for _ in range(5)]
-        visited[i][j] = True
-        while queue:
-            cur_r, cur_c, cur_dist = queue.popleft()
+        visited[r][c] = True
+        while q:
+            cur_r, cur_c, cur_dist = q.popleft()
             for i in range(4):
                 next_r = cur_r + dr[i]
                 next_c = cur_c + dc[i]
                 next_dist = cur_dist + 1
-                if inRange(next_r, next_c) and place[next_r][next_c] != 'X' and not visited[next_r][next_c]:
+                if in_Range(next_r, next_c) and not visited[next_r][next_c] and place[next_r][next_c] != 'X':
                     if next_dist > 2:
                         continue
                     elif place[next_r][next_c] == 'P':
                         return False
                     else:
-                        queue.append((next_r, next_c, next_dist))
+                        q.append((next_r, next_c, next_dist))
                         visited[next_r][next_c] = True
         return True
-            
+                
+    
+    # one place code
+    def one_place(place):
+        for r in range(5):
+            for c in range(5):
+                if place[r][c] == 'P':
+                    if not bfs(r, c, place):
+                        return False
+        return True
+    
+    
+    # main code
     answer = []
     for place in places:
-        result = 0
-        p = 0
-        for i in range(5):
-            for j in range(5):
-                if place[i][j] == 'P':
-                    p += 1
-                    result += bfs(i, j, place)
-        if result == p:
+        if one_place(place):
             answer.append(1)
         else:
             answer.append(0)
     return answer
+    
