46e0990db34262895a1c9cbb3fc8ec8982f4901f
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-07-25 04:32:17 +0900

    [Gold IV] Title: 연구소, Time: 2464 ms, Memory: 34132 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,63 +1,63 @@
 import sys
-import copy
+from itertools import combinations
+from collections import deque
+
 input = sys.stdin.readline
 row_len, col_len = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(row_len)]
 
-dr = [0, 1, 0, -1]
-dc = [1, 0, -1, 0]
+def in_Range(r, c):
+    return 0 <= r < row_len and 0 <= c < col_len
 
-def dfs(new_graph, r, c):
-    visited[r][c] = True 
-    for i in range(4):
-        next_r = r + dr[i]
-        next_c = c + dc[i]
-        if 0 <= next_r < row_len and 0 <= next_c < col_len and new_graph[next_r][next_c] == 0:
-            if not visited[next_r][next_c]:
-                visited[next_r][next_c] = True
-                new_graph[next_r][next_c] = 2
-                dfs(new_graph, next_r, next_c)
-    return new_graph
+# bfs/dfs code
+def bfs(graph):
+    answer = 0
+    new_graph = [graph[i][:] for i in range(row_len)]
+    q = deque(virus)
 
-# 0 위치
-location = []
+    while q:
+        cur_r, cur_c = q.popleft()
+        for dr, dc in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
+            next_r = cur_r + dr
+            next_c = cur_c + dc
+            if in_Range(next_r, next_c):
+                if new_graph[next_r][next_c] == 0:
+                    new_graph[next_r][next_c] = 2
+                    q.append((next_r, next_c))
+
+    # 0 개수 
+    for r in range(row_len):
+        for c in range(col_len):
+            if new_graph[r][c] == 0:
+                answer += 1
+    return answer
+
+
+# main code
+    # 벽 3개 선택
+    # dfs/bfs
+    # max update
+empty = []
+virus = []
 for r in range(row_len):
     for c in range(col_len):
         if graph[r][c] == 0:
-            location.append((r, c))
-# 3개 뽑기 (조합)
-answer = []
-def backtracking(start_index, curr):
-    if len(curr) == 3:
-        answer.append(curr[:])
-        return 
-    for i in range(start_index, len(location)):
-        curr.append(location[i])
-        backtracking(i+1, curr)
-        curr.pop()
-    return answer
-result = backtracking(0, [])
+            empty.append((r, c))
+        elif graph[r][c] == 2:
+            virus.append((r, c))
 
-import copy
-final = 0
-for l in result:
-    new_graph = copy.deepcopy(graph)
-    # 벽 세우기
-    new_graph[l[0][0]][l[0][1]] = 1
-    new_graph[l[1][0]][l[1][1]] = 1
-    new_graph[l[2][0]][l[2][1]] = 1
+result = 0
+for virus_cand in combinations(empty, 3):
+    # 3개의 벽 선택
+    # 벽을 새로 세운다.
+    for i in range(3):
+        graph[virus_cand[i][0]][virus_cand[i][1]] = 1
+    # bfs 수행
+    bfs_result = bfs(graph)
+    if bfs_result > result:
+        result = bfs_result
+    # 세운 벽을 다시 되돌린다.
+    for i in range(3):
+        graph[virus_cand[i][0]][virus_cand[i][1]] = 0
 
-    # 새로 벽을 세운 그래프에 대해 탐색
-    n_result = 0
-    visited = [[False]*col_len for _ in range(row_len)]
-    for r in range(row_len):
-        for c in range(col_len):
-            if new_graph[r][c] == 2 and visited[r][c] == False:
-                new_graph = dfs(new_graph, r, c)
-    for i in range(row_len):
-        for j in range(col_len):
-            if new_graph[i][j] == 0:
-                n_result += 1
-    if n_result > final:
-        final = n_result
-print(final)
+print(result)
