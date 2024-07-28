b2b1b54967f882b3c2e0a75436f9401138aeb1cb
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-07-28 16:31:33 +0900

    [Gold III] Title: 연구소 3, Time: 756 ms, Memory: 34136 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.py
+++ 백준/Gold/17142. 연구소 3/연구소 3.py
@@ -1,78 +1,71 @@
 import sys
-from itertools import combinations
 from collections import deque 
+from itertools import combinations 
 
+# 입력 받기
 input = sys.stdin.readline
 N, M = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
+row_len = col_len = N
+board = [list(map(int, input().split())) for _ in range(row_len)]
 
-# graph를 초기화한다.
-    # 0 빈칸 -> '_'
-    # 1 벽 -> '-'
-    # 2 바이러스 -> 비활성 상태 '*'
+# 초기 설정
+dr = [0, 1, 0, -1]
+dc = [1, 0, -1, 0]
+
+# board 전처리
 virus = []
-for r in range(N):
-    for c in range(N):
-        if graph[r][c] == 0:
-            graph[r][c] = '_'
-        elif graph[r][c] == 1:
-            graph[r][c] = '-'
+for r in range(row_len):
+    for c in range(col_len):
+        if board[r][c] == 0:
+            board[r][c] = '_'
+        elif board[r][c] == 1:
+            board[r][c] = '-'
         else:
-            graph[r][c] = '*'
-            virus.append([r, c])
+            board[r][c] = '*'
+            virus.append((r, c))
 
-# bfs 
-# 큐 생성하고, M개의 위치와 시간0을 입력
-# 큐 반복
-    # 상하좌우 시간 + 1씩
-# 다 끝났을 때 이중 for문 돌면서 '_'(빈칸)이 있으면 -1 리턴하고 없으면 최댓값 리턴
+# bfs
 def in_Range(r, c):
-    return 0 <= r < N and 0 <= c < N
+    return 0 <= r < row_len and 0 <= c < col_len
 
 def bfs(v):
-    level = 0
-    dr = [0, 1, 0, -1]
-    dc = [1, 0, -1, 0]
+    # v : [[r, c], ..., [r, c]]
+    new_board = [board[i][:] for i in range(row_len)]
     q = deque()
-    for v_r, v_c in v:  
-        q.append([v_r, v_c, 0])
-    new_graph = [graph[i][:] for i in range(N)]
+    result = 0
+    for v_r, v_c in v:
+        q.append((v_r, v_c, 0))
+        new_board[v_r][v_c] = 0
+    
     while q:
-        cur_r, cur_c, cur_l = q.popleft()
+        cur_r, cur_c, cur_n = q.popleft() #1, 5, 0
         for i in range(4):
             next_r = cur_r + dr[i]
             next_c = cur_c + dc[i]
-            next_l = cur_l + 1
-            if in_Range(next_r, next_c):
-                if new_graph[next_r][next_c] == '_':
-                    new_graph[next_r][next_c] = next_l
-                    level = max(level, next_l)
-                    q.append([next_r, next_c, next_l])
-                elif new_graph[next_r][next_c] == '*':
-                    new_graph[next_r][next_c] = next_l 
-                    q.append([next_r, next_c, next_l])
+            next_n = cur_n + 1
+            if in_Range(next_r, next_c) :
+                if new_board[next_r][next_c] == '_':
+                    q.append((next_r, next_c, next_n))
+                    new_board[next_r][next_c] = next_n
+                    result = max(next_n, result)
+                elif new_board[next_r][next_c] == '*':
+                    q.append((next_r, next_c, next_n))
+                    new_board[next_r][next_c] = next_n
 
-    for r in range(N):
-        for c in range(N):
-            if new_graph[r][c] == '_':
-                return -1
-    return level
+    for r in range(row_len):
+        for c in range(col_len):
+            if new_board[r][c] == '_':
+                return -1 
+    return result
+
 
 # main
-# 바이러스의 위치를 담는다.
-# 바이러스의 위치들 중 조합을 통해 M개 선택한다.
-# M개 선택된 바이러스를 활성 상태로 바꾼다.
-# bfs()
-# 다시 비활성 상태로 바꾼다. 
-final_level = -1
+final_result = -1
 for v in combinations(virus, M):
-    for v_r, v_c in v:
-        graph[v_r][v_c] = 0
-    level = bfs(v)
-    if final_level == -1 and level != -1:
-        final_level = level
-    elif final_level != -1 and level != -1:
-        final_level = min(final_level, level)
-    for v_r, v_c in v:
-        graph[v_r][v_c] = '*'
-print(final_level)
+    result = bfs(v)
+    if result != -1 and final_result != -1:
+        final_result = min(result, final_result)
+    elif result != -1 and final_result == -1:
+        final_result = result
+
+print(final_result)
