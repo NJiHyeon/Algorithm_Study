942cc90711a13c2fcf292eea4b25a9badbe91e1f
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-07-28 23:01:15 +0900

    [level 3] Title: 블록 이동하기, Time: 44.29 ms, Memory: 13.3 MB -BaekjoonHub

--- 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.py
+++ 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.py
@@ -1,48 +1,64 @@
 def solution(board):
-    from collections import deque
-    def in_Range(lx, ly, rx, ry):
-        return 0 <= lx < n and 0 <= ly < n and 0 <= rx < n and 0 <= ry < n
-
-    def get_next_pos(cur_pos, board):
-        next_pos = []
-        pos = list(cur_pos)
-        lx, ly, rx, ry = pos[0][0], pos[0][1], pos[1][0], pos[1][1]
-        # 상하좌우 확인
-        for dx, dy in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
-            nlx, nly, nrx, nry = lx+dx, ly+dy, rx+dx, ry+dy
-            if in_Range(nlx, nly, nrx, nry):
-                if board[nlx][nly] == 0 and board[nrx][nry] == 0:
-                    next_pos.append(set([(nlx, nly), (nrx, nry)]))
-        
-        # 가로 방향 - 회전 확인
-        if lx == rx:
-            for i in [1, -1]:
-                if in_Range(lx+i, ly, rx+i, ry):
-                    if board[lx+i][ly] == 0 and board[rx+i][ry] == 0:
-                        next_pos.append(set([(lx, ly), (lx+i, ly)]))
-                        next_pos.append(set([(rx, ry), (rx+i, ry)]))
-
-        # 세로 방향 - 회전 확인
-        if ly == ry:
-            for i in [1, -1]:
-                if in_Range(lx, ly+i, rx, ry+i):
-                    if board[lx][ly+i] == 0 and board[rx][ry+i]:
-                        next_pos.append(set([(lx, ly), (lx, ly+i)]))
-                        next_pos.append(set([(rx, ry), (rx, ry+i)]))
-        return next_pos
+    from collections import deque 
     
-    n = len(board)
-    robot_pos = set([(0, 0), (0, 1)])
+    # 초기 설정
+    row_len, col_len = len(board), len(board[0])
+    def in_Range(r, c):
+        return 0 <= r < row_len and 0 <= c < col_len and board[r][c] == 0
+    
+    # main = bfs 
     q = deque()
-    q.append((robot_pos, 0))
+    q.append([(0, 0), (0, 1), 0])
     visited = set()
-    visited.add(frozenset(robot_pos))
+    visited.add(frozenset(((0, 0), (0, 1))))
     while q:
-        cur_pos, cost = q.popleft()
-        if (n-1, n-1) in cur_pos:
-            return cost
-        for next_pos in get_next_pos(cur_pos, board):
-            if frozenset(next_pos) not in visited:
-                q.append((next_pos, cost+1))
-                visited.add(frozenset(next_pos))
-    return -1
+        cur1, cur2, cur_n = q.popleft()
+        cur_r1, cur_c1 = cur1[0], cur1[1]
+        cur_r2, cur_c2 = cur2[0], cur2[1]
+        if (cur_r1, cur_c1) == (row_len-1, col_len-1) or (cur_r2, cur_c2) == (row_len-1, col_len-1):
+            return cur_n
+        
+        for dr, dc in [[0, 1], [1, 0], [0, -1], [-1, 0]]:  
+            next_r1 = cur_r1 + dr
+            next_c1 = cur_c1 + dc
+            next_r2 = cur_r2 + dr
+            next_c2 = cur_c2 + dc
+            next_n = cur_n + 1
+            if in_Range(next_r1, next_c1) and in_Range(next_r2, next_c2):
+                if frozenset(((next_r1, next_c1), (next_r2, next_c2))) not in visited:
+                    q.append([(next_r1, next_c1), (next_r2, next_c2), next_n])
+                    visited.add(frozenset(((next_r1, next_c1), (next_r2, next_c2))))
+        
+        # 가로 방향
+        if cur_r1 == cur_r2 :
+            for dr, dc in [[1, 0], [-1, 0]]:
+                next_r1 = cur_r1 + dr
+                next_c1 = cur_c1 + dc
+                next_r2 = cur_r2 + dr
+                next_c2 = cur_c2 + dc
+                next_n = cur_n + 1
+                if in_Range(next_r1, next_c1) and in_Range(next_r2, next_c2):
+                    if frozenset(((cur_r1, cur_c1), (next_r1, next_c1))) not in visited:
+                        q.append([(cur_r1, cur_c1), (next_r1, next_c1), next_n])
+                        visited.add(frozenset(((cur_r1, cur_c1), (next_r1, next_c1))))
+                    if frozenset(((cur_r2, cur_c2), (next_r2, next_c2))) not in visited:
+                        q.append([(cur_r2, cur_c2), (next_r2, next_c2), next_n])
+                        visited.add(frozenset(((cur_r2, cur_c2), (next_r2, next_c2))))
+        
+        # 세로 방향
+        if cur_c1 == cur_c2 :
+            for dr, dc in [[0, 1], [0, -1]]:
+                next_r1 = cur_r1 + dr
+                next_c1 = cur_c1 + dc
+                next_r2 = cur_r2 + dr
+                next_c2 = cur_c2 + dc
+                next_n = cur_n + 1
+                if in_Range(next_r1, next_c1) and in_Range(next_r2, next_c2):
+                    if frozenset(((cur_r1, cur_c1), (next_r1, next_c1))) not in visited:
+                        q.append([(cur_r1, cur_c1), (next_r1, next_c1), next_n])
+                        visited.add(frozenset(((cur_r1, cur_c1), (next_r1, next_c1))))
+                    if frozenset(((cur_r2, cur_c2), (next_r2, next_c2))) not in visited:
+                        q.append([(cur_r2, cur_c2), (next_r2, next_c2), next_n])
+                        visited.add(frozenset(((cur_r2, cur_c2), (next_r2, next_c2))))
+    
+    
