4c0b2e2e40f350a9c0421c3a3b3a9157fcfed7dc
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-07-28 15:19:38 +0900

    [Gold IV] Title: 연구소, Time: 2364 ms, Memory: 34148 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,63 +1,75 @@
 import sys
-from itertools import combinations
 from collections import deque
+from itertools import combinations
 
+# 입력값 받기
 input = sys.stdin.readline
 row_len, col_len = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(row_len)]
+board = [list(map(int, input().split())) for _ in range(row_len)]
 
-def in_Range(r, c):
-    return 0 <= r < row_len and 0 <= c < col_len
-
-# bfs/dfs code
-def bfs(graph):
-    answer = 0
-    new_graph = [graph[i][:] for i in range(row_len)]
-    q = deque(virus)
-
-    while q:
-        cur_r, cur_c = q.popleft()
-        for dr, dc in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
-            next_r = cur_r + dr
-            next_c = cur_c + dc
-            if in_Range(next_r, next_c):
-                if new_graph[next_r][next_c] == 0:
-                    new_graph[next_r][next_c] = 2
-                    q.append((next_r, next_c))
-
-    # 0 개수 
-    for r in range(row_len):
-        for c in range(col_len):
-            if new_graph[r][c] == 0:
-                answer += 1
-    return answer
-
-
-# main code
-    # 벽 3개 선택
-    # dfs/bfs
-    # max update
-empty = []
-virus = []
+# 필요한 것 정의
+wall_cand = []
+virus_cand = []
 for r in range(row_len):
     for c in range(col_len):
-        if graph[r][c] == 0:
-            empty.append((r, c))
-        elif graph[r][c] == 2:
-            virus.append((r, c))
+        if board[r][c] == 0:
+            wall_cand.append((r, c))
+        elif board[r][c] == 2:
+            virus_cand.append((r, c))
 
+def in_Range(r, c):
+    return 0 <= r < row_len and 0 <= c < col_len 
+
+# bfs
+def bfs(board):
+    dr = [0, 1, 0, -1]
+    dc = [1, 0, -1, 0]
+    new_board = [board[i][:] for i in range(row_len)]
+    visited = [[False]*col_len for _ in range(row_len)]
+    # 퍼뜨리기
+    q = deque()
+    for virus_r, virus_c in virus_cand:
+        if not visited[virus_r][virus_c]:
+            q.append((virus_r, virus_c))
+            visited[virus_r][virus_c] = True 
+            while q:
+                cur_r, cur_c = q.popleft()
+                for i in range(4):
+                    next_r = cur_r + dr[i]
+                    next_c = cur_c + dc[i]
+                    if in_Range(next_r, next_c) and new_board[next_r][next_c] == 0:
+                        if not visited[next_r][next_c]:
+                            q.append((next_r, next_c))
+                            visited[next_r][next_c] = True 
+                            new_board[next_r][next_c] = 2
+    
+    
+    # 안전영역 개수 세기
+    safe_region = 0
+    for r in range(row_len):
+        for c in range(col_len):
+            if new_board[r][c] == 0:
+                safe_region += 1
+    return safe_region
+
+
+
+
+# main
 result = 0
-for virus_cand in combinations(empty, 3):
-    # 3개의 벽 선택
-    # 벽을 새로 세운다.
-    for i in range(3):
-        graph[virus_cand[i][0]][virus_cand[i][1]] = 1
-    # bfs 수행
-    bfs_result = bfs(graph)
-    if bfs_result > result:
-        result = bfs_result
-    # 세운 벽을 다시 되돌린다.
-    for i in range(3):
-        graph[virus_cand[i][0]][virus_cand[i][1]] = 0
+for wall in combinations(wall_cand, 3):
+    # wall : [(r1, c1), (r2, c2), (r3, c3)]
+    # 벽 세우기
+    for wr, wc in wall:
+        board[wr][wc] = 1
+
+    # bfs로 바이러스 퍼뜨리기
+    safe_region = bfs(board)
+    result = max(result, safe_region)
+
+    # 다시 벽 되돌리기
+    for wr, wc in wall:
+        board[wr][wc] = 0
+
 
 print(result)
