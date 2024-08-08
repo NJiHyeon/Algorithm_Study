87d1322e84ea9b36f7777f05c4cb46f114f22023
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-08-08 12:40:07 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.20 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,44 +1,20 @@
 def solution(new_id):
-    # new_id : "...!@BaT#*..y.abcdefghijklm"	
-    spec =['~','!','@','#','$','%','^','&','*','(',')','=','+','[','{',']','}',':','?',',','<','>','/']
-    answer = ''
-    '''1단계'''
-    new_id = new_id.lower()
-    '''2단계'''
-    for i in range(len(new_id)):
-        if new_id[i] not in spec:
-            answer += new_id[i]
-    new_id = answer
-    '''3단계'''
-    while '..' in new_id:
-        new_id = new_id.replace('..', '.')
-    
-    '''4단계'''
-    l = len(new_id)
-    if l == 0:
-        pass
-    elif l == 1 and new_id[0] == '.':
-        new_id = ''
-        l -= 1
-    else:
-        if new_id[0] == '.':
-            new_id = new_id[1:]
-            l -= 1
-        if new_id[-1] == '.':
-            new_id = new_id[:-1]
-            l -= 1
-    
-    '''5-7단계'''
-    if l == 0:
-        return 'aaa'
-    elif l >= 15:
-        new_id = new_id[:15]
-        if new_id[-1] != '.':
-            return new_id
-        else:
-            return new_id[:-1]
-    elif l == 1:
-        return new_id + new_id[-1]*2
-    elif l == 2:
-        return new_id + new_id[-1]
-    return new_id
+    import re
+    # "...!@BaT#*..y.abcdefghijklm"
+    step1 = new_id.lower()
+    step2 = re.sub('[^a-z0-9-_.]', '', step1)
+    while '..' in step2:
+        step2 = step2.replace('..', '.')
+    step3 = step2[:]
+    step4 = step3.strip('.')
+    if step4 == '':
+        step4 += 'a'
+    step5 = step4[:]
+    if len(step5) >= 16:
+        step5 = step5[:15]
+    step6 = step5.rstrip('.')
+    while len(step6) <= 2:
+        step6 += step6[-1]
+    step7 = step6[:]
+    return step7
+        
