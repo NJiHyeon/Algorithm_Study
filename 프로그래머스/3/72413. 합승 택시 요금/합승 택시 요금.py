fd4844cb11a155c2be2ef42d952b49e5b3ba4849
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-08-15 13:42:45 +0900

    [level 3] Title: 합승 택시 요금, Time: 29.10 ms, Memory: 15.4 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -2,15 +2,16 @@ def solution(n, s, a, b, fares):
     import heapq
     graph = {}
     for fare in fares:
-        if fare[0] not in graph:
-            graph[fare[0]] = [(fare[1], fare[2])]
+        aa, bb, cc = fare[0], fare[1], fare[2]
+        if aa not in graph:
+            graph[aa] = [(bb, cc)]
         else:
-            graph[fare[0]].append((fare[1], fare[2]))
-        if fare[1] not in graph:
-            graph[fare[1]] = [(fare[0], fare[2])]
+            graph[aa].append((bb, cc))
+        if bb not in graph:
+            graph[bb] = [(aa, cc)]
         else:
-            graph[fare[1]].append((fare[0], fare[2]))
-
+            graph[bb].append((aa, cc))
+    
     distances = [[float('inf')] * (n+1) for _ in range(3)]
     distances[0][s] = distances[1][a] = distances[2][b] = 0
     
@@ -25,7 +26,7 @@ def solution(n, s, a, b, fares):
                 if next_dist < distances[i][next_v]:
                     distances[i][next_v] = next_dist
                     heapq.heappush(pq, (next_dist, next_v))
-                    
+        
     answer = float('inf')
     for i in range(1, n+1):
         answer = min(answer, distances[0][i]+distances[1][i]+distances[2][i])
