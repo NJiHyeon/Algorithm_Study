cb4418cbaf142a5db1901e6ed4950c2269fde1cf
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-08-15 13:09:42 +0900

    [level 3] Title: 등산코스 정하기, Time: 6081.44 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
@@ -1,48 +1,37 @@
 def solution(n, paths, gates, summits):
     import heapq
-    #🧊 가중치 그래프 만들기
     summits.sort()
-    summit_set = set(summits)
     
     graph = {}
     for path in paths:
         a, b, c = path[0], path[1], path[2]
-        if a in graph:
-            graph[a].append([b, c])
+        if a not in graph:
+            graph[a] = [(b, c)]
         else:
-            graph[a] = [[b, c]]
-        
-        if b in graph:
-            graph[b].append([a, c])
+            graph[a].append((b, c))
+        if b not in graph:
+            graph[b] = [(a, c)]
         else:
-            graph[b] = [[a, c]]
+            graph[b].append((a, c))
+    distances = [10000000] * (n+1)
+
     pq = []
-    visited = [10000001] * (n+1)
-    
-    #🧊 모든 출입구를 우선순위 큐에 삽입한다. 
     for gate in gates:
         heapq.heappush(pq, (0, gate))
-        visited[gate] = 0
+        distances[gate] = 0
     
-    #🧊 intensity를 기준으로 다익스트라를 진행한다. 
     while pq:
-        intensity, node = heapq.heappop(pq)
-        if intensity > visited[node] or node in summit_set:
-            continue 
-        
-        for next_node, weight in graph[node]:
-            # 하나의 길 과정에서는 최댓값 구함
-            next_intensity = max(weight, intensity)
-            # 단, 각 등산로랑 비교할때는 최소값 저장
-            if next_intensity < visited[next_node]:
-                visited[next_node] = next_intensity
-                heapq.heappush(pq, (next_intensity, next_node))
-            
-    #🧊 다익스트라 완료 후 산봉우리들을 순회하며 정답을 찾는다. 
-    # (순서대로 정렬되어 있으므로 같을 떄는 생각 안해도 된다.)
-    min_intensity = [0, 10000001]
-    for summit in summits:
-        if min_intensity[1] > visited[summit]:
-            min_intensity[0] = summit
-            min_intensity[1] = visited[summit]
-    return min_intensity
+        cur_dist, cur_v = heapq.heappop(pq)
+        if distances[cur_v] < cur_dist or cur_v in summits:
+            continue
+        for next_v, cost in graph[cur_v]:
+            next_dist = max(distances[cur_v], cost)
+            if next_dist < distances[next_v]:
+                distances[next_v] = next_dist
+                heapq.heappush(pq, (next_dist, next_v))
+    
+    min_distance = [0, 10000001]
+    for s in summits:
+        if distances[s] < min_distance[1]:
+            min_distance = [s, distances[s]]
+    return min_distance
