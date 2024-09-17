4b03479b9cdcc5656288f88cbcd6f008668cfbde
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-09-18 03:23:47 +0900

    [level 2] Title: 피로도, Time: 152.87 ms, Memory: 15.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,14 +1,26 @@
 def solution(k, dungeons):
-    n_list = []
-    
-    import itertools
-    nPr = itertools.permutations(dungeons, len(dungeons))
-    for d in nPr :
-        kd = k
-        n = 0
-        for j in range(len(d)) :
-            if kd >= d[j][0] :
-                kd -= d[j][1]
-                n += 1
-        n_list.append(n)
-    return max(n_list)
+    final_result = 0
+    # 1. 순서를 담은 순열
+    permutations = []
+    def backtracking(curr):
+        if len(curr) == len(dungeons):
+            permutations.append(curr[:])
+            return
+        for i in range(len(dungeons)):
+            if i not in curr:
+                curr.append(i)
+                backtracking(curr)
+                curr.pop()
+    backtracking([])
+    # 2. 탐험할 수 있는 최대 던전 개수
+    for permut in permutations:
+        current = k-0
+        result = 0
+        for i in permut:
+            if current >= dungeons[i][0]:
+                current -= dungeons[i][1]
+                result += 1
+        if result > final_result:
+            final_result = result
+    return final_result
+        
