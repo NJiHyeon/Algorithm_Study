630476e2b46451bcf697b59b73830f2565102bff
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-09-19 16:39:00 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 102.81 ms, Memory: 19.9 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,30 +1,22 @@
 def solution(queue1, queue2):
-    from collections import deque
-    m = 4*len(queue1)
-    queue1 = deque(queue1)
-    queue2 = deque(queue2)
-    l = sum(queue1)
-    r = sum(queue2)
-    answer = 0
-    n = 0
-    
-    if (l+r) % 2 == 1 :
-        return -1
-    
-    while queue1 and queue2 and n<m :
-        n += 1
-        if l > r :
-            q1 = queue1.popleft()
-            queue2.append(q1)
-            answer += 1
-            l -= q1
-            r += q1
-        elif l < r :
-            q2 = queue2.popleft()
-            queue1.append(q2)
-            answer += 1
-            l += q2
-            r -= q2
-        else :
-            return answer
+    l = 0
+    r = len(queue1)
+    queue = queue1 + queue2
+    left_sum = sum(queue[l:r])
+    right_sum = sum(queue[r:])
+    total_l = len(queue)
+    result = 0
+    while result < 2*total_l:
+        if left_sum < right_sum:
+            left_sum += queue[r]
+            right_sum -= queue[r]
+            r = (r+1)%total_l
+            result += 1
+        elif left_sum > right_sum:
+            left_sum -= queue[l]
+            right_sum += queue[l]
+            l = (l+1)%total_l
+            result += 1
+        else:
+            return result
     return -1
