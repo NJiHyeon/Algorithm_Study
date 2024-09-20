dbba0542c144423e0ff933a4d3a54efafbb2f6a2
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-09-20 19:26:15 +0900

    [level 2] Title: 피로도, Time: 114.63 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,26 +1,21 @@
 def solution(k, dungeons):
-    final_result = 0
-    # 1. 순서를 담은 순열
-    permutations = []
-    def backtracking(curr):
-        if len(curr) == len(dungeons):
-            permutations.append(curr[:])
-            return
+    def backtracking(curr_d, curr_n, k):
+        nonlocal result
+        # 탈출 조건
+        if k >= 0:
+            result = max(result, curr_n)
+
+        # 반복 수행
         for i in range(len(dungeons)):
-            if i not in curr:
-                curr.append(i)
-                backtracking(curr)
-                curr.pop()
-    backtracking([])
-    # 2. 탐험할 수 있는 최대 던전 개수
-    for permut in permutations:
-        current = k
-        result = 0
-        for i in permut:
-            if current >= dungeons[i][0]:
-                current -= dungeons[i][1]
-                result += 1
-        if result > final_result:
-            final_result = result
-    return final_result
-        
+            if i not in curr_d and dungeons[i][0] <= k:
+                curr_d.append(i)
+                k -= dungeons[i][1]
+                curr_n += 1
+                backtracking(curr_d, curr_n, k)
+                curr_d.pop()
+                k += dungeons[i][1]
+                curr_n -= 1
+
+    result = 0
+    backtracking([], 0, k)
+    return result
