d58b0e2b5493205216b52c7eb0fd4fd99b13f7a7
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-09-28 00:31:28 +0900

    [level 3] Title: 등산코스 정하기, Time: 2052.27 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
@@ -1,37 +1,39 @@
 def solution(n, paths, gates, summits):
     import heapq
-    summits.sort()
-    
     graph = {}
     for path in paths:
-        a, b, c = path[0], path[1], path[2]
-        if a not in graph:
-            graph[a] = [(b, c)]
+        if path[0] not in graph:
+            graph[path[0]] = [(path[1], path[2])]
         else:
-            graph[a].append((b, c))
-        if b not in graph:
-            graph[b] = [(a, c)]
+            graph[path[0]].append((path[1], path[2]))
+        
+        if path[1] not in graph:
+            graph[path[1]] = [(path[0], path[2])]
         else:
-            graph[b].append((a, c))
-    distances = [10000000] * (n+1)
+            graph[path[1]].append((path[0], path[2]))
 
     pq = []
+    min_result = [float("inf"), float("inf")]
+    distances = [float("inf")] * (n+1)
+
     for gate in gates:
         heapq.heappush(pq, (0, gate))
         distances[gate] = 0
-    
+
     while pq:
         cur_dist, cur_v = heapq.heappop(pq)
-        if distances[cur_v] < cur_dist or cur_v in summits:
+        if cur_dist > min_result[1]:
+            break
+        if cur_v in summits:
+            if cur_dist < min_result[1]:
+                min_result = [cur_v, cur_dist]
+            if cur_v < min_result[0]:
+                min_result[0] = cur_v
             continue
+
         for next_v, cost in graph[cur_v]:
-            next_dist = max(distances[cur_v], cost)
-            if next_dist < distances[next_v]:
-                distances[next_v] = next_dist
-                heapq.heappush(pq, (next_dist, next_v))
-    
-    min_distance = [0, 10000001]
-    for s in summits:
-        if distances[s] < min_distance[1]:
-            min_distance = [s, distances[s]]
-    return min_distance
+            next_cost = max(distances[cur_v], cost)
+            if next_cost < distances[next_v]:
+                distances[next_v] = next_cost
+                heapq.heappush(pq, (next_cost, next_v))
+    return min_result
