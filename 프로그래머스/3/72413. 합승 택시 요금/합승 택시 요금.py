c3eb6ad1152bd04898c04be94ecc77ea19d4606f
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-09-28 02:43:17 +0900

    [level 3] Title: 합승 택시 요금, Time: 71.74 ms, Memory: 15.2 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,33 +1,35 @@
 def solution(n, s, a, b, fares):
-    import heapq
+    import heapq 
     graph = {}
     for fare in fares:
-        aa, bb, cc = fare[0], fare[1], fare[2]
-        if aa not in graph:
-            graph[aa] = [(bb, cc)]
+        if fare[0] not in graph:
+            graph[fare[0]] = [(fare[1], fare[2])]
         else:
-            graph[aa].append((bb, cc))
-        if bb not in graph:
-            graph[bb] = [(aa, cc)]
+            graph[fare[0]].append((fare[1], fare[2]))
+        if fare[1] not in graph:
+            graph[fare[1]] = [(fare[0], fare[2])]
         else:
-            graph[bb].append((aa, cc))
-    
-    distances = [[float('inf')] * (n+1) for _ in range(3)]
-    distances[0][s] = distances[1][a] = distances[2][b] = 0
-    
-    for i, start_v in enumerate([s, a, b]):
-        pq = [(0, start_v)]
+            graph[fare[1]].append((fare[0], fare[2]))
+
+    taxi = []
+    for start_v in [s, a, b]:
+        distances = [float("inf")]*(n+1)
+        distances[start_v] = 0
+        pq = [(start_v, 0)]
         while pq:
-            cur_dist, cur_v = heapq.heappop(pq)
-            if distances[i][cur_v] < cur_dist:
+            cur_v, cur_cost = heapq.heappop(pq)
+            if distances[cur_v] < cur_cost:
                 continue
             for next_v, cost in graph[cur_v]:
-                next_dist = distances[i][cur_v] + cost
-                if next_dist < distances[i][next_v]:
-                    distances[i][next_v] = next_dist
-                    heapq.heappush(pq, (next_dist, next_v))
-        
-    answer = float('inf')
-    for i in range(1, n+1):
-        answer = min(answer, distances[0][i]+distances[1][i]+distances[2][i])
-    return answer
+                next_cost = distances[cur_v] + cost 
+                if next_cost < distances[next_v]:
+                    distances[next_v] = next_cost
+                    heapq.heappush(pq, (next_v, next_cost))
+        taxi.append(distances[1:])
+    
+    result = float("inf")
+    for i in range(n):
+        if result > (taxi[0][i] + taxi[1][i] + taxi[2][i]):
+            result = taxi[0][i] + taxi[1][i] + taxi[2][i]
+    return result
+
