5290e34ff072fc3114614ab5c5709868589d6830
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-09-28 19:13:16 +0900

    [level 2] Title: 피로도, Time: 92.27 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,21 +1,20 @@
 def solution(k, dungeons):
-    def backtracking(curr_d, curr_n, k):
+    def backtracking(cur_k, cur_n, curr):
+        # 탈출조건
         nonlocal result
-        # 탈출 조건
-        if k >= 0:
-            result = max(result, curr_n)
+        result = max(result, cur_n)
 
-        # 반복 수행
-        for i in range(len(dungeons)):
-            if i not in curr_d and dungeons[i][0] <= k:
-                curr_d.append(i)
-                k -= dungeons[i][1]
-                curr_n += 1
-                backtracking(curr_d, curr_n, k)
-                curr_d.pop()
-                k += dungeons[i][1]
-                curr_n -= 1
-
+        # 반복수행
+        for d in range(len(dungeons)):
+            if d not in curr:
+                if cur_k >= dungeons[d][0]:
+                    curr.append(d)
+                    cur_k -= dungeons[d][1]
+                    cur_n += 1
+                    backtracking(cur_k, cur_n, curr)
+                    cur_k += dungeons[d][1]
+                    cur_n -= 1
+                    curr.pop()
     result = 0
-    backtracking([], 0, k)
+    backtracking(k, 0, [])
     return result
