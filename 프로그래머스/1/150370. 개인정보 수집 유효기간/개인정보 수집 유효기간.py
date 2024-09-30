be14b1dff358a586164977dd70f43bad6937d9f1
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-09-30 22:02:11 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.32 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,38 +1,27 @@
 def solution(today, terms, privacies):
     result = []
-    terms_dict = {term.split(' ')[0] : int(term.split(' ')[1]) for term in terms}
-    for i in range(len(privacies)):
-        start_day, m = privacies[i].split(' ')[0], terms_dict[privacies[i].split(' ')[1]]
-        
-        # 날짜 계산
-        start_y = int(start_day.split('.')[0])
-        start_m = int(start_day.split('.')[1]) + m
-        start_d = int(start_day.split('.')[2]) - 1
-        
-        if start_m > 12:
-            if start_m % 12 != 0:
-                start_y += (start_m // 12)
-                start_m = (start_m % 12)
-            else:
-                start_y += (start_m // 12 - 1)
-                start_m = 12
-        
-        if start_d == 0:
-            start_d = 28
-            start_m -= 1
-            if start_m == 0:
-                start_m = 12
-                start_y -= 1
+    terms_dict = {}
+    for term in terms:
+        term_ = term.split(" ")
+        terms_dict[term_[0]] = int(term_[1])
 
-        
-        # 자릿수
-        if len(str(start_m)) == 1:
-            start_m = f'0{start_m}'
-        if len(str(start_d)) == 1:
-            start_d = f'0{start_d}'
-        
-        end_day = f'{start_y}.{start_m}.{start_d}'
-    
-        if today > end_day:
+    def cal(tmp, select):
+        if select == 'p':
+            ymd, t = tmp.split(" ")[0], tmp.split(" ")[1]
+            y, m, d = int(ymd.split(".")[0]), int(ymd.split(".")[1]), int(ymd.split(".")[2])
+            m += terms_dict[t]
+            day_cal = (y-2000)*336 + m*28 + d
+        else:
+            ymd = tmp.split(".")
+            y, m, d = int(ymd[0]), int(ymd[1]), int(ymd[2])
+            day_cal = (y-2000)*336 + m*28 + d
+        return day_cal
+            
+    today_cal = cal(today, 't')
+    for i, privacy in enumerate(privacies):
+        cur_cal = cal(privacy, 'p')
+        if (today_cal - cur_cal) >= 0:
             result.append(i+1)
     return result
+    
+    
