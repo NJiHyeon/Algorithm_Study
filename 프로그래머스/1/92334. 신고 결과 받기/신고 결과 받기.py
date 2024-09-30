5d3106685748ddbbdb40442a91652b99f73b1879
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-09-30 21:24:18 +0900

    [level 1] Title: 신고 결과 받기, Time: 576.37 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,34 +1,21 @@
 def solution(id_list, report, k):
-    # id_list = ["muzi", "frodo", "apeach", "neo"]
-    # report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
-    # k = 2
-    report_dict = {id_list[i] : set() for i in range(len(id_list))}
-    singo_dict = {}
-
-    for i in range(len(report)):
-        li = report[i].split(' ')
-        a, b = li[0], li[1]
-        if b in report_dict[a]:
+    result = []
+    talking = {id:[] for id in id_list}
+    talked = {id:0 for id in id_list}
+    for tmp in report:
+        ing, ed = tmp.split(" ")[0], tmp.split(" ")[1]
+        if ed in talking[ing]:
             pass
         else:
-            report_dict[a].add(b)
-            if b not in singo_dict:
-                singo_dict[b] = 1
-            else:
-                singo_dict[b] += 1
-            
-    result = []
+            talking[ing].append(ed)
+            talked[ed] += 1
+    
     for id in id_list:
-        # muzi
-        mail = 0
-        singo_id_list = report_dict[id] #["frodo", "neo"]
-        if singo_id_list:
-            for singo_id in singo_id_list:
-                if singo_dict[singo_id] >= k:
-                    mail += 1
-        result.append(mail)
-        
+        n = 0
+        for ed in talking[id]:
+            if talked[ed] >= k:
+                n += 1
+        result.append(n)
     return result
-        
-        
+            
         
