fb24b38c51f2367043592a21e6847ddf51473004
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-10-03 19:31:01 +0900

    [level 3] Title: 양과 늑대, Time: 20.23 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,24 +1,24 @@
 def solution(info, edges):
+    result = []
+    visit = [False] * len(info)
+    
     def backtracking(sheep, wolf):
         # 탈출조건
         if sheep > wolf:
-            output.append(sheep)
+            result.append(sheep)
         else:
             return
-
         # 반복수행
-        for p, c in edges:
-            if visited[p] and not visited[c]:
-                visited[c] = True
-                if info[c] == 0:
+        for edge in edges:
+            if visit[edge[0]] == True and visit[edge[1]] == False:
+                if info[edge[1]] == 0:
+                    visit[edge[1]] = True
                     backtracking(sheep+1, wolf)
+                    visit[edge[1]] = False
                 else:
+                    visit[edge[1]] = True
                     backtracking(sheep, wolf+1)
-                visited[c] = False
-                
-
-    output = []
-    visited = [False] * len(info)
-    visited[0] = True
+                    visit[edge[1]] = False
+    visit[0] = True
     backtracking(1, 0)
-    return max(output)
+    return max(result)
