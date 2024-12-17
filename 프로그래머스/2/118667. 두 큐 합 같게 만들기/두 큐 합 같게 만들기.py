f6a47d49ba4afc74fd8c6c0a5a59688d7dd112c0
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2024-12-17 15:38:03 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 116.01 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,22 +1,24 @@
 def solution(queue1, queue2):
+    left = sum(queue1)
+    right = sum(queue2)    
+    queue = queue1 + queue2
+    L = len(queue)
+    cur_l = 0
     l = 0
     r = len(queue1)
-    n = 0
-    N = len(queue1) + len(queue2)
-    target = (sum(queue1) + sum(queue2)) // 2
-    queue = queue1 + queue2
-    cur_sum = sum(queue[l:r])
-    
-    while n < 2*N:
-        if cur_sum == target:
-            return n
-        elif cur_sum < target:
-            cur_sum += queue[r]
+    while cur_l < 2*L:
+        if left == right:
+            return cur_l
+        elif left < right:
+            left += queue[r]
+            right -= queue[r]
             r += 1
-            r %= N
-        elif cur_sum > target:
-            cur_sum -= queue[l]
+            r %= L
+            cur_l += 1
+        elif left > right:
+            left -= queue[l]
+            right += queue[l]
             l += 1
-            l %= N
-        n += 1
+            l %= L
+            cur_l += 1
     return -1
