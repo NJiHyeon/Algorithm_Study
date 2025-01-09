b7a3259ec2ba579fdf3aeeeb27f01147e19de7b7
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2025-01-09 19:22:59 +0900

    [level 2] Title: 후보키, Time: 3.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.py
+++ 프로그래머스/2/42890. 후보키/후보키.py
@@ -1,43 +1,48 @@
 def solution(relation):
+    col_name = 'abcdefgh'[:len(relation[0])]
+    # 부분 집합
+    def backtrack(curr, cur_i, k):
+        # base case
+        if len(curr) == k:
+            combi.append(curr[:])  
+        # repeat
+        for j in range(cur_i, len(col_name)):
+            curr.append(col_name[j])
+            backtrack(curr, j+1, k)
+            curr.pop()
     combi = []
-    category = [str(i) for i in range(len(relation[0]))]
-    def backtrack(cur, cur_i, n):
-        if len(cur) == n:
-            combi.append(cur[:])
-        for i in range(cur_i, len(relation[0])):
-            cur.append(category[i])
-            backtrack(cur, i+1, n)
-            cur.pop()
-    for n in range(1, len(relation[0])+1):
-        backtrack([], 0, n)
+    for k in range(1, len(col_name)+1):
+        backtrack([], 0, k)
 
-    # 테이블 만들기 (딕셔너리로 구성)
-    candidate_key = {}
-    for i in range(len(relation)):
-        for j in range(len(relation[0])):
-            if str(j) not in candidate_key:
-                candidate_key[str(j)] = [relation[i][j]]
+    # Table
+    student_table = {}
+    for r in range(len(relation)):
+        for c in range(len(relation[0])):
+            if col_name[c] not in student_table:
+                student_table[col_name[c]] = [relation[r][c]]
             else:
-                candidate_key[str(j)].append(relation[i][j])
+                student_table[col_name[c]].append(relation[r][c])
 
-    # 유일성+최소성 판단
     output = []
-    for cur_cate in combi:
-        # 유일성
-        curr = []
-        for i in range(len(relation)):
-            k = ''
-            for c in cur_cate:
-                k += candidate_key[c][i]
-            curr.append(k)
-        if len(curr) != len(list(set(curr))):
-            continue
+    # 유일성
+    for cur_combi in combi: #['b', 'c']
         # 최소성
-        mini = 'Y'
-        for o in output:
-            if set(o).issubset(set(cur_cate)):
-                mini = 'N'
+        ok = True
+        for out in output:
+            if set(out).issubset(set(cur_combi)):
+                ok = False
                 break
-        if mini == 'Y':
-            output.append(cur_cate)
-    return len(output)
+        if ok == False:
+            continue
+        students = []
+        for r in range(len(relation)):
+            student = ''
+            for c in cur_combi:
+                student += student_table[c][r]
+            students.append(student)
+        if len(relation) == len(set(students)): #유일성만족
+            output.append(cur_combi)
+            
+    return len(output)           
+            
+            
