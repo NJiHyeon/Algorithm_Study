90af58644c93e0b0063e9f8b84429318e81a2488
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2025-02-01 22:35:34 +0900

    [level 2] Title: 후보키, Time: 3.21 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/README.md
+++ 프로그래머스/2/42890. 후보키/README.md
@@ -1,10 +1,10 @@
 # [level 2] 후보키 - 42890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42890) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42890?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 3.13 ms
+메모리: 10.2 MB, 시간: 3.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 09일 19:22:53
+2025년 02월 01일 22:35:29
 
 ### 문제 설명
 
--- 프로그래머스/2/42890. 후보키/후보키.py
+++ 프로그래머스/2/42890. 후보키/후보키.py
@@ -1,48 +1,36 @@
 def solution(relation):
-    col_name = 'abcdefgh'[:len(relation[0])]
-    # 부분 집합
-    def backtrack(curr, cur_i, k):
-        # base case
-        if len(curr) == k:
-            combi.append(curr[:])  
-        # repeat
-        for j in range(cur_i, len(col_name)):
-            curr.append(col_name[j])
-            backtrack(curr, j+1, k)
-            curr.pop()
+    col = 'ABCDEFGH'
+    dict = {col[i]:i for i in range(len(relation[0]))}
+    candidate_key = []
+    
+    # 1. 조합 만들기
     combi = []
-    for k in range(1, len(col_name)+1):
-        backtrack([], 0, k)
-
-    # Table
-    student_table = {}
-    for r in range(len(relation)):
-        for c in range(len(relation[0])):
-            if col_name[c] not in student_table:
-                student_table[col_name[c]] = [relation[r][c]]
-            else:
-                student_table[col_name[c]].append(relation[r][c])
-
-    output = []
-    # 유일성
-    for cur_combi in combi: #['b', 'c']
-        # 최소성
-        ok = True
-        for out in output:
-            if set(out).issubset(set(cur_combi)):
-                ok = False
-                break
-        if ok == False:
-            continue
-        students = []
-        for r in range(len(relation)):
-            student = ''
-            for c in cur_combi:
-                student += student_table[c][r]
-            students.append(student)
-        if len(relation) == len(set(students)): #유일성만족
-            output.append(cur_combi)
-            
-    return len(output)           
-            
-            
+    def backtrack(cur, cur_i, n):
+        if len(cur) == n:
+            combi.append(''.join(cur[:]))
+            return
+        for i in range(cur_i, len(relation[0])):
+            cur.append(col[i])
+            backtrack(cur, i+1, n)
+            cur.pop()
+    for n in range(1, len(relation[0])+1):
+        backtrack([], 0, n)
+    
+    # 2. 최소성 확인
+    for new_key in combi:
+        uniq = []
+        mini = True
+        for cand in candidate_key:
+            if set(cand).issubset(set(new_key)):
+                mini = False
+        # 3. 유일성 확인
+        if mini == True:
+            for r in range(len(relation)):
+                p = ''
+                for c in new_key:
+                    p += relation[r][dict[c]]
+                uniq.append(p)
+            if len(list(set(uniq))) == len(uniq):
+                candidate_key.append(new_key)
+    return len(candidate_key)
+    
