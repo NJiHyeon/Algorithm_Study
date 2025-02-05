bb8a0b803bf64cdbc0f87ad783211323d119818b
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2025-02-05 13:53:49 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 85.39 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,24 +1,28 @@
 def solution(queue1, queue2):
-    left = sum(queue1)
-    right = sum(queue2)    
-    queue = queue1 + queue2
-    L = len(queue)
-    cur_l = 0
-    l = 0
-    r = len(queue1)
-    while cur_l < 2*L:
-        if left == right:
-            return cur_l
-        elif left < right:
-            left += queue[r]
-            right -= queue[r]
-            r += 1
-            r %= L
-            cur_l += 1
-        elif left > right:
-            left -= queue[l]
-            right += queue[l]
-            l += 1
-            l %= L
-            cur_l += 1
-    return -1
+    from collections import deque 
+    queue1 = deque(queue1)
+    queue2 = deque(queue2)
+    cur1 = sum(queue1)
+    cur2 = sum(queue2)
+    target = (cur1+cur2)//2
+    result = 0
+    l = 2*(len(queue1) + len(queue2)) 
+    
+    while result < l:
+        if cur1 == cur2:
+            return result 
+        elif cur1 > cur2:
+            c1 = queue1.popleft()
+            queue2.append(c1)
+            cur1 -= c1
+            cur2 += c1
+            result += 1
+        else:
+            c2 = queue2.popleft()
+            queue1.append(c2)
+            cur1 += c2
+            cur2 -= c2
+            result += 1
+    if result == l:
+        return -1
+    return result
