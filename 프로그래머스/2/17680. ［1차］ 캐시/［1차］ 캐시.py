6cc430dee1574df5917d1f3ffaf5dac23ad231bd
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2025-02-09 20:58:21 +0900

    [level 2] Title: [1차] 캐시, Time: 73.40 ms, Memory: 17.5 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,27 +1,24 @@
 def solution(cacheSize, cities):
+    from collections import deque 
+    
     answer = 0
-    cache = []
-    if cacheSize == 0 :
-        answer = 5*len(cities)
-    else :
-        for city in cities :
-            city=city.lower()
-            if len(cache) < cacheSize :
-                if city in cache :
-                    answer += 1
-                    cache.pop(cache.index(city))
-                    cache.append(city)
-                else :
-                    answer += 5
-                    cache.append(city)
-            else :
-                if city in cache :
-                    answer += 1
-                    cache.pop(cache.index(city))
-                    cache.append(city)
-                else :
-                    answer += 5
-                    cache.pop(0)
-                    cache.append(city)
-            
+    cache_list = deque()
+    
+    if cacheSize == 0:
+        return 5 * len(cities)
+    
+    for city in cities:
+        city = city.lower()
+        if city in cache_list:
+            # i = cities.index(city)
+            cache_list.remove(city)
+            cache_list.append(city)
+            answer += 1
+        else:
+            if len(cache_list) < cacheSize:
+                cache_list.append(city)
+            else:
+                cache_list.popleft()
+                cache_list.append(city)
+            answer += 5
     return answer
