8165f50071271de42304cc6ca9ebe165104d6529
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2025-02-16 05:37:08 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.10 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,39 +1,34 @@
 def solution(places):
-    dr = [0, 1, 0, -1]
-    dc = [1, 0, -1, 0]
+    from collections import deque
 
-    def dfs(cur_r, cur_c, curr_n):
-        # 탈출 조건
-        if curr_n >= 2:
-            return
-        # 반복 수행
+    def dfs(r, c, n, place, visit):
+        dr = [0, 1, 0, -1]
+        dc = [1, 0, -1, 0]
+
+        visit[(r, c)] = True 
         for i in range(4):
-            next_r = cur_r + dr[i]
-            next_c = cur_c + dc[i]
+            next_r = r + dr[i]
+            next_c = c + dc[i]
             if 0 <= next_r < 5 and 0 <= next_c < 5:
-                if not visited[next_r][next_c]:
+                if (next_r, next_c) not in visit:
                     if place[next_r][next_c] == 'P':
-                        return False
-                    elif place[next_r][next_c] == 'O':
-                        visited[next_r][next_c] = True
-                        if dfs(next_r, next_c, curr_n+1) == False:
-                            return False
-                        visited[next_r][next_c] = False
-        return True
+                        return 0
+                    elif place[next_r][next_c] == 'O' and n == 0:
+                        if dfs(next_r, next_c, n+1, place, visit) == 0:
+                            return 0
+        return 1
 
 
-    answer = []
-    for p in range(5):
-        place = places[p]
-        result = 1
-        visited = [[False]*5 for _ in range(5)]
+    def main(place):
+        visit = {}
         for r in range(5):
             for c in range(5):
-                if place[r][c] == "P":
-                    visited[r][c] = True
-                    if not dfs(r, c, 0):
-                        result = 0
-                    visited[r][c] = False
-        answer.append(result)
+                if place[r][c] == 'P' and (r, c) not in visit:
+                    if dfs(r, c, 0, place, visit) == 0:
+                        return 0
+        return 1
 
-    return answer
+    result = []
+    for i in range(5):
+        result.append(main(places[i]))
+    return result
