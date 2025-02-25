2240d37c174d9756d0388fcf7e2e4bcc583f85ef
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2025-02-25 13:20:50 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.66 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,21 +1,25 @@
 def solution(jobs):
-    import heapq 
+    import heapq
+    # start 시간이 짧은순대로 정렬 
     jobs.sort()
     heap = []
-    current_time, completed_jobs, total_response_time = 0, 0, 0
+    current_time, completed_jobs, total_time = 0, 0, 0
     jobs_idx = 0
-
+    
     while completed_jobs < len(jobs):
+        # 현재 시점에서 처리할 수 있는 작업 모두 추가
         while jobs_idx < len(jobs) and jobs[jobs_idx][0] <= current_time:
             start, duration = jobs[jobs_idx]
             heapq.heappush(heap, (duration, start))
             jobs_idx += 1
-
+            
+        # 추가된 작업 처리하기 
         if heap:
             duration, start = heapq.heappop(heap)
-            total_response_time += (current_time - start + duration)
+            total_time += (current_time - start + duration)
             current_time += duration
             completed_jobs += 1
         else:
             current_time = jobs[jobs_idx][0]
-    return total_response_time // completed_jobs
+            
+    return total_time // len(jobs)
