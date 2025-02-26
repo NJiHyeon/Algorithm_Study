095443df3e1b7656293e756f341a128cd6e1923c
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2025-02-26 19:39:07 +0900

    [level 3] Title: 등산코스 정하기, Time: 8847.33 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
@@ -1,39 +1,46 @@
 def solution(n, paths, gates, summits):
+    # summits 돌고 gates 안에 있는 노드가 나오면 return 
+    # pop : 작은거부터
+    # next 값 : max(현재 노드의 값, 연결된 선의 값)
+    # next 노드 : 저장된 값보다 작으면 작은 값으로 바꾸기 (이 길로 올 수 있다는 거니까)
     import heapq
-    graph = {}
-    for path in paths:
-        if path[0] not in graph:
-            graph[path[0]] = [(path[1], path[2])]
-        else:
-            graph[path[0]].append((path[1], path[2]))
+    graph = {i : [] for i in range(1, n+1)}
+    for u in range(len(paths)):
+        i, j, w = paths[u][0], paths[u][1], paths[u][2]
+        graph[i].append((j, w))
+        graph[j].append((i, w))
         
-        if path[1] not in graph:
-            graph[path[1]] = [(path[0], path[2])]
-        else:
-            graph[path[1]].append((path[0], path[2]))
+    def dijkstra(graph, start_v, n):
+        times = [float("inf")] * (n+1)
+        times[start_v] = 0
+        pq = [(0, start_v)]
+        
+        while pq:
+            cur_time, cur_v = heapq.heappop(pq)
+            #저장된 값이 이미 더 작으면 볼 필요 없음.
+            if cur_v in gates:
+                return cur_time
+            if times[cur_v] < cur_time or (cur_v != start_v and cur_v in summit_set): 
+                continue
+                
+            for next_v, time in graph[cur_v]:
+                next_time = max(times[cur_v], time)
+                if times[next_v] > next_time :
+                    times[next_v] = next_time
+                    heapq.heappush(pq, (next_time, next_v))
+        return 10000001
 
-    pq = []
-    min_result = [float("inf"), float("inf")]
-    distances = [float("inf")] * (n+1)
-
-    for gate in gates:
-        heapq.heappush(pq, (0, gate))
-        distances[gate] = 0
-
-    while pq:
-        cur_dist, cur_v = heapq.heappop(pq)
-        if cur_dist > min_result[1]:
-            break
-        if cur_v in summits:
-            if cur_dist < min_result[1]:
-                min_result = [cur_v, cur_dist]
-            if cur_v < min_result[0]:
-                min_result[0] = cur_v
-            continue
-
-        for next_v, cost in graph[cur_v]:
-            next_cost = max(distances[cur_v], cost)
-            if next_cost < distances[next_v]:
-                distances[next_v] = next_cost
-                heapq.heappush(pq, (next_cost, next_v))
-    return min_result
+    result = []
+    summit_set = set(summits)
+    for summit in summits:
+        o = dijkstra(graph, summit, n)
+        heapq.heappush(result, (o, summit))
+    min_time, summit = heapq.heappop(result)
+    return [summit, min_time]
+        
+        
+        
+        
+        
+        
+        
