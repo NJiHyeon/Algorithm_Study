d8d372e2e648b073b4718884a5cf105f81079190
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2025-02-28 16:43:39 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.14 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,27 +1,18 @@
 def solution(today, terms, privacies):
-    result = []
-    terms_dict = {}
-    for term in terms:
-        term_ = term.split(" ")
-        terms_dict[term_[0]] = int(term_[1])
+    terms_dict = {term[0]:int(term[2:]) for term in terms}
 
-    def cal(tmp, select):
-        if select == 'p':
-            ymd, t = tmp.split(" ")[0], tmp.split(" ")[1]
-            y, m, d = int(ymd.split(".")[0]), int(ymd.split(".")[1]), int(ymd.split(".")[2])
-            m += terms_dict[t]
-            day_cal = (y-2000)*336 + m*28 + d
-        else:
-            ymd = tmp.split(".")
-            y, m, d = int(ymd[0]), int(ymd[1]), int(ymd[2])
-            day_cal = (y-2000)*336 + m*28 + d
-        return day_cal
-            
-    today_cal = cal(today, 't')
-    for i, privacy in enumerate(privacies):
-        cur_cal = cal(privacy, 'p')
-        if (today_cal - cur_cal) >= 0:
+    def yyyymmdd_to_dd(day, category=None):
+        y, m, d = day.split('.')
+        to_dd = (int(y)-2000)*12*28 + (int(m)-1)*28 + (int(d)-1)
+        if category:
+            to_dd += terms_dict[category]*28
+        return to_dd
+        
+    result = []
+    today = yyyymmdd_to_dd(today)
+    for i in range(len(privacies)):
+        start_day, category = privacies[i].split(" ")
+        end_day = yyyymmdd_to_dd(start_day, category)
+        if end_day <= today:
             result.append(i+1)
     return result
-    
-    
