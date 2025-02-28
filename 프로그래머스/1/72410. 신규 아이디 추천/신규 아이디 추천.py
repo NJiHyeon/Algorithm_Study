1b801c0e1b4e7e9a7caf69e4afd4c3b39c61b4e1
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2025-02-28 14:51:29 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.26 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,19 +1,40 @@
 def solution(new_id):
     import re
-    new_id = new_id.lower() #1
-    new_id = re.sub('[^a-z0-9-_.]', '', new_id) #2
-    while '..' in new_id: #3
-        new_id = new_id.replace('..', '.')
-    if new_id and new_id[0] == '.':
-        new_id = new_id[1:]
-    if new_id and new_id[-1] == '.':
-        new_id = new_id[:-1]
-    if len(new_id) == 0:
-        new_id = "a"
-    if len(new_id) >= 16:
-        new_id = new_id[:15]
-        if new_id[-1] == '.':
-            new_id = new_id[:-1]
-    while len(new_id) <= 2:
-        new_id += new_id[-1]
-    return new_id
+    new_id1 = new_id.lower()
+    
+    new_id2 = re.sub('[^a-z0-9-_.]', '', new_id1)
+    
+    while '..' in new_id2:
+        new_id2 = new_id2.replace('..', '.')
+    new_id3 = new_id2
+    
+    if new_id3 and new_id3[0] == '.':
+        if len(new_id3) == 1:
+            new_id3 = ''
+        else:
+            new_id3 = new_id3[1:]
+    if new_id3 and new_id3[-1] == '.':
+        if len(new_id3) == 1:
+            new_id3 = ''
+        else:
+            new_id3 = new_id3[:-1]
+    new_id4 = new_id3
+    
+    if new_id4 == '':
+        new_id4 = 'a'
+    new_id5 = new_id4
+        
+    if len(new_id5) >= 16:
+        new_id5 = new_id5[:15]
+        if new_id5[-1] == '.':
+            new_id5 = new_id5[:-1]
+    new_id6 = new_id5
+    
+    while len(new_id6) <= 2:
+        new_id6 += new_id6[-1]
+    new_id7 = new_id6
+    
+    return new_id7
+    
+
+    return new_id3
