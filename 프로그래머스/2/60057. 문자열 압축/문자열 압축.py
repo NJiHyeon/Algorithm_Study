aaf92993aab2e33019fb5375206f6b8a4a5e9772
nozzy <100822150+NJiHyeon@users.noreply.github.com>
2025-02-28 12:44:50 +0900

    [level 2] Title: 문자열 압축, Time: 5.86 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,23 +1,28 @@
 def solution(s):
-    min_s = 1001
-    for l in range(1, len(s)+1):
-        stack = []
-        l_s = ''
-        for i in range(0, len(s), l):
-            cur_s = s[i : i+l]
-            if (stack and stack[-1] != cur_s):
-                past_n = 0
-                while stack:
-                    past_s = stack.pop()
-                    past_n += 1
-                if past_n == 1:
-                    l_s += past_s 
+    min_l = 1001
+    for unit in range(1, len(s)+1): #단위
+        cur_s = ''
+        cur_n = 0
+        unit_s = ''
+        for index in range(0, len(s), unit): #앞에서부터
+            if cur_s == '':
+                cur_s = s[index : index+unit]
+                cur_n = 1
+            elif cur_s == s[index : index+unit]:
+                cur_n += 1
+            else:
+                if cur_n == 1:
+                    unit_s += cur_s
                 else:
-                    l_s += (str(past_n)+past_s)
-            stack.append(cur_s)
-        if len(stack) == 1:
-            l_s += stack[-1]
-        elif len(stack) >= 2:
-            l_s += (str(len(stack))+stack[-1])
-        min_s = min(min_s, len(l_s))
-    return min_s
+                    unit_s += (str(cur_n)+cur_s)
+                cur_n = 1
+                cur_s = s[index : index+unit]
+        
+        if cur_n == 1:
+            unit_s += cur_s
+        else:
+            unit_s += (str(cur_n)+cur_s)
+                
+        min_l = min(min_l, len(unit_s))
+    return min_l
+            
